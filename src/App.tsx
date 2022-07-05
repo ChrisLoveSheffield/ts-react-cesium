bc0e9585e89345784da6ca47ecc38f4dd3160dda
Chunyuan Huang <chris.huang1@aecom.com>
2022-07-05 09:23:29 +0800

    test

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Viewer } from 'resium';
+
+function App() {
+  return <Viewer full />;
+}
+
+export default App;
